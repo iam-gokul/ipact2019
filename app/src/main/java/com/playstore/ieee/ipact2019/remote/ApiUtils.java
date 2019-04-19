7b7b585034fdce72a21800d2a9642728fb5598ee
Gokula Krishnan <32614778+iam-gokul@users.noreply.github.com>
2019-04-19 21:28:53 +0530

    Delete ApiUtils.java

--- app/src/main/java/com/playstore/ieee/ipact2019/remote/ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.playstore.ieee.ipact2019.remote;
-
-public class ApiUtils {
-    public static final String BASE_URL = "https://ipactconference.com/api/";
-
-    public static Api getUserService(){
-        return RetrofitClient.getClient(BASE_URL).create(Api.class);
-    }
-}
